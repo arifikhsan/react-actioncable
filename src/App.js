f72044cc2c66b864c2b795dbdb1a168c518597a2
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-05-30 17:11:26 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ActionCable from "actioncable";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { text: "" };
+
+  componentDidMount() {
+    window.fetch("http://localhost:3000/notes/1").then((data) => {
+      data.json().then((res) => {
+        this.setState({ text: res.text });
+      });
+    });
+
+    const cable = ActionCable.createConsumer("ws://localhost:3000/cable");
+    this.sub = cable.subscriptions.create("NoteChannel", {
+      received: this.handleReceiveNewText,
+    });
+  }
+
+  handleReceiveNewText = ({ text }) => {
+    if (text !== this.state.text) {
+      this.setState({ text });
+    }
+  };
+
+  handleChange = (e) => {
+    this.setState({ text: e.target.value });
+    this.sub.send({ text: e.target.value, id: 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.text}</h1>
+        <textarea
+          value={this.state.text}
+          onChange={this.handleChange}
+        ></textarea>
+      </div>
+    );
+  }
 }
 
 export default App;
